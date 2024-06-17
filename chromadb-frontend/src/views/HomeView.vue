e245db6417095d026864470a12f42e9e9e007abe
treeleaves30760 <treeleaves30760@gmail.com>
2024-06-17 15:46:02 +0800

    chore: Update page title and add input form for querying documents

--- chromadb-frontend/src/views/HomeView.vue
+++ chromadb-frontend/src/views/HomeView.vue
@@ -2,6 +2,58 @@
 </script>
 
 <template>
-  <main>
-  </main>
+  <input type="text" v-model="inputText" class="form-control my-3" placeholder="Enter text" @input="Query_Document">
+  <!-- <button class="btn btn-primary" @click="Show_Document">Submit</button> -->
+  <div v-for="(document, index) in documents" :key="index" class="card m-2">
+    <div class="card-body">
+      <div>
+        <vue-markdown :source="document" />
+      </div>
+    </div>
+  </div>
 </template>
+
+<script>
+import { ref } from 'vue';
+import VueMarkdown from 'vue-markdown-render'
+
+export default {
+  setup() {
+    const inputText = ref('');
+    const documents = ref([]);
+
+    const Query_Document = async () => {
+      try {
+        const response = await fetch('http://127.0.0.1:5000/query_document', {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({ content: inputText.value })
+        });
+        const data = await response.json();
+
+        if (data.results && data.results.documents) {
+          documents.value = data.results.documents[0];
+        }
+
+        console.log('Documents',documents.value);
+      } catch (error) {
+        console.error('Error fetching documents:', error);
+      }
+    };
+
+    const Show_Document = () => {
+      console.log(documents.value);
+    }
+
+    return {
+      inputText,
+      documents,
+      Query_Document,
+      Show_Document,
+    };
+  },
+  components: {
+    VueMarkdown
+  },
+};
+</script>
